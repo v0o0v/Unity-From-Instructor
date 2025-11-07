2fd7adf9b404c69241b085ba9bab797a078f1137
JeongDay <cidd1332@gmail.com>
2025-11-07 15:58:33 +0900

    2025.11.07 금요일 - 오후 시간 1
    - 2D 프로젝트 셋팅
    - Jump Cat 리소스 다운로드
    - Collider & Rigidbody

---
+++ Assets/02. Scripts/Cat/LoopMap.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LoopMap : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    
+    public float offsetSpeed = 1f;
+    
+    void Start()
+    {
+        meshRenderer = this.GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        Vector2 offset = Vector2.right * offsetSpeed * Time.deltaTime;
+
+        meshRenderer.material.SetTextureOffset("_BaseMap", meshRenderer.material.mainTextureOffset + offset);
+    }
+}
